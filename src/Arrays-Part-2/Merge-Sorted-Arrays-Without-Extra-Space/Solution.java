b683ef10e53d5d23448350d0a93ccc9776cb05ec
Manvi Chaddha <60390722+manvi0308@users.noreply.github.com>
2022-06-23 11:53:17 +0000

    Structure added

---
+++ src/Arrays-Part-2/Merge-Sorted-Arrays-Without-Extra-Space/Solution.java
@@ -0,0 +1,12 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+    public static int[] ninjaAndSortedArrays(int arr1[], int arr2[], int m, int n) {
+        // Write your code here.
+        int p1 = m - n + 1, p2 = n;
+        
+
+        
+        return arr1;
+    }
+}
---
+++ src/Arrays-Part-2/Rotate-Matrix/Solution.java
@@ -0,0 +1,9 @@
+import java.util.* ;
+import java.io.*; 
+import java.util.ArrayList;
+
+public class Solution {
+    public static void rotateMatrix(ArrayList<ArrayList<Integer>> mat, int n, int m) {
+        // Write your code here.
+    }
+}
