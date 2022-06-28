4f1d8ddb43386090f24dbc8af43d2a4aed7a06ad
Manvi Chaddha <60390722+manvi0308@users.noreply.github.com>
2022-06-28 11:35:30 +0000

    Added

--- Readme.md
+++ Readme.md
@@ -34,6 +34,7 @@
 1. Find Middle Element of Linked List
 2. Reverse Linked list
 3. Merge two sorted linked lists
+4. Add two numbers represented by Linked lists
 
 ## Dynamic Programming
 
---
+++ src/LinkedLists/AddNumbers.java
@@ -0,0 +1,53 @@
+public class AddNumbers {
+    class LinkedListNode {
+        int data;
+        LinkedListNode next;
+
+        public LinkedListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    /*
+     * The trick is here we somehow need to get access to the head
+     * of the sum linked list
+     * 
+     * To do so we will first create a dummy node
+     * That dummy node will be pointed to by temp pointer
+     * //! That is temp--->dummy
+     * Now as we will sum the nodes the temp pointer will be used
+     * In the end the scenario would be
+     * //! dummy --> s1 ---> s2 ---> s3---> -1
+     * //!                            |
+     * //!                           temp
+     * //! 
+     * Return the dummy.next
+     */
+    static LinkedListNode addTwoNumbers(LinkedListNode head1, LinkedListNode head2) {
+        // Write your code here.
+        LinkedListNode dummy = new LinkedListNode();
+        LinkedListNode temp = dummy;
+        int carry = 0;
+
+        while (head1 != null || head2 != null || carry == 1) {
+            int sum = 0;
+            if (head1 != null) {
+                sum = sum + head1.data;
+                head1 = head1.next;
+            }
+
+            if (head2 != null) {
+                sum = sum + head2.data;
+                head2 = head2.next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            LinkedListNode node = new LinkedListNode(sum % 10);
+            temp.next = node;
+            temp = temp.next;
+
+        }
+
+        return dummy.next;
+    }
+}
